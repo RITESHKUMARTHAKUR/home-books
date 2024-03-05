f1c53e8fe18447572efb36e56d0ab2cabd6566f2
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2024-03-06 00:22:58 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const upload = multer({ dest: 'uploads/' })
 const PORT = process.env.PORT || 5000;
 const origin = process.env.ORIGIN_FRONTEND;
 
-app.use(cors({credentials: true, origin: origin }));
+// app.use(cors({credentials: true, origin: origin }));
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
